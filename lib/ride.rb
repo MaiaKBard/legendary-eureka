ff4ea393f940b0a2bdde0d9184fa0f9df1270784
Maia Bard <Maiakbard@gmail.com>
2025-03-10 09:13:19 -0600

    Add code to initilaize and create attr_reader methods for name, distnace, and terrain

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,11 @@
 class Ride
+  attr_reader :name, :distance, :terrain
+
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @loop = info[:loop]
+    @terrain = info[:terrain]
+  end
 
 end
